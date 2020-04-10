62df055eb3fc4a89d18c12d628b1a6c6f552d778
Mário Prada <mariosilvaprada@gmail.com>
2020-04-10 12:51:29 +0100

    updating

--- src/routes/App/App.styled.jsx
+++ src/routes/App/App.styled.jsx
@@ -5,8 +5,7 @@ export const Container = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
   grid-gap: 1rem;
   padding: 2rem;
-  background-color: #b1bfd8;
-  background-image: linear-gradient(315deg, #b1bfd8 0%, #6782b4 74%);
+  
 `;
 
 export const DataWrapper = styled.div``;
