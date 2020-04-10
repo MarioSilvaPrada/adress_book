196379787eb4109da8663db9996baf226468c7b1
Mário Prada <mariosilvaprada@gmail.com>
2020-04-10 20:28:16 +0100

    adding filter

--- src/routes/Settings/Settings.styled.jsx
+++ src/routes/Settings/Settings.styled.jsx
@@ -1,11 +1,7 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-    padding-top: 5rem;
-`;
+export const Wrapper = styled.div`padding-top: 5rem;`;
 
 export const Title = styled.div`color: red;`;
 
-export const Container = styled.div`
-
-`;
+export const Container = styled.div``;
