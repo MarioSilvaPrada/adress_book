b86a9ff0240de1f525cafff49c1156428836960d
Mário Prada <mariosilvaprada@gmail.com>
2020-04-11 00:24:22 +0100

    adding filter and infinite scroll

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -9,40 +9,37 @@ import * as S from './Home.styled';
 
 const Home = ({
   users, isLoading, filteredName, filteredCountry, fetchMore,
-}) => {
- 
-  return isLoading ? (
-    <Spinner />
-  ) : (
-    <InfiniteScroll
-      dataLength={users.length}
-      next={fetchMore}
-      hasMore
-      loader={<Spinner />}
-      endMessage={<b>End of users catalog</b>}
-    >
-      <S.Container>
-        {users
-          .filter((user) => {
-            const userName = `${user.name.first} ${user.name.last}`;
-            return userName.toLowerCase().includes(filteredName.toLowerCase());
-          })
-          .filter((user) => user.nat.includes(filteredCountry))
-          .map(({
-            name, email, location, picture, login, cell,
-          }) => (
-            <UserCard
-              key={cell}
-              name={name}
-              email={email}
-              location={location}
-              picture={picture}
-              login={login}
-            />
-          ))}
-      </S.Container>
-    </InfiniteScroll>
-  );
-};
+}) => (isLoading ? (
+  <Spinner />
+) : (
+  <InfiniteScroll
+    dataLength={users.length}
+    next={fetchMore}
+    hasMore
+    loader={<Spinner />}
+    endMessage={<b>End of users catalog</b>}
+  >
+    <S.Container>
+      {users
+        .filter((user) => {
+          const userName = `${user.name.first} ${user.name.last}`;
+          return userName.toLowerCase().includes(filteredName.toLowerCase());
+        })
+        .filter((user) => user.nat.includes(filteredCountry))
+        .map(({
+          name, email, location, picture, login, cell,
+        }) => (
+          <UserCard
+            key={cell}
+            name={name}
+            email={email}
+            location={location}
+            picture={picture}
+            login={login}
+          />
+        ))}
+    </S.Container>
+  </InfiniteScroll>
+));
 
 export default Home;
